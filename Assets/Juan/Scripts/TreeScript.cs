f6e42e5703121a1b9ed63b2f7abb96441129604a
elMaromo <juampats12345@gmail.com>
2023-02-05 03:21:47 +0100

    added last scene

--- Assets/Juan/Scripts/TreeScript.cs
+++ Assets/Juan/Scripts/TreeScript.cs
@@ -7,8 +7,15 @@ namespace juan
     public class TreeScript : MonoBehaviour
     {
         public Casilla tipe;
+        public float sizeIncrease;
 
         private Roots roots;
+        private SpriteRenderer sp;
+
+        public void Awake()
+        {
+            sp = GetComponent<SpriteRenderer>();
+        }
 
         public void initRoots(Roots iRoots)
         {
@@ -19,6 +26,16 @@ namespace juan
         private void OnMouseDown()
         {
             roots.Expand(tipe);
+        }
+
+        public void OnMouseEnter()
+        {
+            transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
+        }
+
+        public void OnMouseExit()
+        {
+            transform.localScale -= new Vector3(sizeIncrease, sizeIncrease, 0);
         }
     }
 }
